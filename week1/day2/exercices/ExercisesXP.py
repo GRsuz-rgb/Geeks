396e7f440cd92c265ffe3fce7af8d46f1330f143
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-08-12 16:53:06 +0200

    ex2

--- week1/day2/exercices/ExercisesXP.py
+++ week1/day2/exercices/ExercisesXP.py
@@ -1,3 +1,18 @@
-#Exercise1 : Convert lists into dictionaries
+#Exercise 1 : Convert lists into dictionaries
 keys = ['Ten', 'Twenty', 'Thirty']
 values = [10, 20, 30]
+
+print(dict(zip(keys, values)))
+
+#Exercise 2 : Cinemax #2
+#A movie theater charges different ticket prices depending on a person’s age.
+
+family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+#1 How much does each family member have to pay ?
+for name, age in family.items():
+    if age < 3:
+        print(f"{name} ticket is free")
+    elif 3 <= age <= 12:
+        print(f"{name} pays: $10")
+    else:
+        print(f"{name} pays: $15")
