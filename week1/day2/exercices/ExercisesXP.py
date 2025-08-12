67309396dbbf56e320723e6c42eae2289eb33188
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-08-12 17:14:38 +0200

    ex2 finished

--- week1/day2/exercices/ExercisesXP.py
+++ week1/day2/exercices/ExercisesXP.py
@@ -1,8 +1,11 @@
 #Exercise 1 : Convert lists into dictionaries
+"""
 keys = ['Ten', 'Twenty', 'Thirty']
 values = [10, 20, 30]
 
 print(dict(zip(keys, values)))
+
+"""
 
 #Exercise 2 : Cinemax #2
 #A movie theater charges different ticket prices depending on a person’s age.
@@ -16,3 +19,29 @@ for name, age in family.items():
         print(f"{name} pays: $10")
     else:
         print(f"{name} pays: $15")
+
+#2 Print out the family’s total cost for the movies.
+total = 0
+for age in family.values():
+    if age < 3:
+        continue
+    elif 3 <= age <= 12:
+        total += 10
+    else:
+        total += 15
+
+print(f"The total cost for the family is: ${total}")
+
+#3 Bonus: Ask the user to input the names and ages instead of using the provided family variable 
+# (Hint: ask the user for names and ages and add them into a family dictionary that is initially empty).
+user_family = {}
+while True:
+    name = input("Enter family member's name (or tape x to finish) : ")
+    if name.lower() == 'x':
+        break
+    age = int(input(f"Enter {name}'s age: "))
+    user_family[name] = age
+print("User's family:", user_family)
+
+
+
