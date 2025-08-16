d758f3e843ed30528ef7c83bb4bee890a6269974
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-08-16 15:56:45 +0200

    ex4 done

--- week1/day4/exercices/exercice4.py
+++ week1/day4/exercices/exercice4.py
@@ -3,16 +3,32 @@
 #last_name : (string)
 
 class Family():
-    def __init__(self, members, last_name):
-        self.members = members
+    def __init__(self, last_name):
+        self.members = [] 
         self.last_name = last_name
 
 #Implement the following methods:
-#born: adds a child to the members list (use **kwargs), don’t forget to print a message congratulating the family.
-#is_18: takes the name of a family member as a parameter and returns True if they are over 18 and False if not.
-#family_presentation: a method that prints the family’s last name and all the members’ details.
-    def born(self, **member):
-        
+#born:
+    def born(self, **dic_member):
+        self.members.append(dic_member)
+        print(f"Congratulation {self.last_name} the new member")
+    
+#is_18:  
+    def is_18(self, member_name):
+        for member in self.members:
+            if member.get('name') == member_name:
+                if member.get('age', 0) >= 18:
+                    return  True
+                else:
+                    return False
+                
+#family_presentation:     
+    def family_presentation(self):
+        print(f"Family last name : {self.last_name}")
+        for member in self.members:
+            print(f"name : {member.get('name')} , age : {member.get('age')} , gender : {member.get('gender')}")
 
+            
 
-        
+#Create an instance of the Family class, with the last name of your choice, and the below members. Then call all the methods you created in Point 2.
+
