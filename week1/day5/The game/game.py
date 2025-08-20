8a2d705aaba7af37ed9b7b0dd19c6e0a6b7a0339
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-08-21 00:24:05 +0200

    game commit

--- week1/day5/The game/game.py
+++ week1/day5/The game/game.py
@@ -1,1 +1,41 @@
 #Game class
+import random
+
+class Game:
+    def get_user_item(self):
+        valid_items = ['rock', 'paper', 'scissors']
+        while True:
+            user_choice = input("Select rock, paper, or scissors: ").lower()
+            if user_choice in valid_items:
+                return user_choice
+            print("Invalid choice! Please select rock, paper, or scissors.")
+
+    def get_computer_item(self):
+        return random.choice(['rock', 'paper', 'scissors'])
+
+    def get_game_result(self, user_item, computer_item):
+        if user_item == computer_item:
+            return 'draw'
+        winning_combinations = {
+            'rock': 'scissors',
+            'paper': 'rock',
+            'scissors': 'paper'
+        }
+        if winning_combinations[user_item] == computer_item:
+            return 'win'
+        return 'loss'
+
+    def play(self):
+        user_item = self.get_user_item()
+        computer_item = self.get_computer_item()
+        result = self.get_game_result(user_item, computer_item)
+        
+        print(f"You selected {user_item}. The computer selected {computer_item}. ", end="")
+        if result == 'win':
+            print("You win!")
+        elif result == 'loss':
+            print("You lose!")
+        else:
+            print("You drew!")
+            
+        return result
