519a1a1e4d0bd383a139875fb3dc352b7058819c
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-09-02 12:58:48 +0200

    challenge2

---
+++ week3/day1/exercices/dailyChallenge2/challenge2.js
@@ -0,0 +1,16 @@
+//1. 2. 3.
+for(let i=0; i <= 6; i++){
+    console.log('*'.repeat(i));
+}
+
+let j = 0;
+while( j <= 6) {
+    console.log('*'.repeat(j));
+    j++;
+}
+
+let k = 0;
+do{
+    console.log('*'.repeat(k));
+    k++;
+}while(k <= 6)
