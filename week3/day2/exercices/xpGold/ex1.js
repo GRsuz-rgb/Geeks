88cf5b765d9ea34b2b539e0b828c6c3a8f4a53dd
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-09-04 21:48:23 +0200

    commit

---
+++ week3/day2/exercices/xpGold/ex1.js
@@ -0,0 +1,18 @@
+//Exercise 1 : is_Blank
+function isBlank(ch){
+   return ch.trim().length === 0 ? true : false; 
+}
+
+console.log(isBlank(''));
+console.log(isBlank('abc'));
+
+
+
+
+
+
+
+
+
+
+
