42444c90fa2cebbdd56ba705d7843f9a6af1a1ca
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-09-06 00:15:56 +0200

    commit

--- week2/day4/dailyChallenge/insert_countries.py
+++ week2/day4/dailyChallenge/insert_countries.py
@@ -1,10 +1,19 @@
-import requests
+import requirements
 import psycopg2
 import random
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
 
+def get_connection():
+    return psycopg2.connect(
+        dbname = os.getenv("PGDATABASE", "countries"),
+        user = os.getenv("PGUSER", "postgres"),
+        password = os.dotenv("PGPASSWORD", "postgresql"),
+        host = os.getenv("PGHOST", "localhost"),
+        port = os.getenv("PGPORT", 5432)
+    )
 
 
 
