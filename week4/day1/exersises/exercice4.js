0c0f0c0a708a42fe830f69d47cc9188b2bbf5b89
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-09-12 18:40:44 +0200

    commit ex4

---
+++ week4/day1/exersises/exercice4.js
@@ -0,0 +1,7 @@
+//Exercise 4 : Find the sum
+
+//Create a one line function (ie. an arrow function) that receives two numbers as parameters and returns the sum.
+const sum = (x, y) => x + y;
+
+
+console.log(sum(3, 6));
