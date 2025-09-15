4eac8eeac1496369ad8862c4deae0f0ba9185649
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-09-15 11:56:07 +0200

    commit ex3Ninja

---
+++ week4/day2/XpNinja/ex3.js
@@ -0,0 +1,20 @@
+//Exercise 3 : Employees #3
+
+
+const users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
+             { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
+             { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
+             { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
+             { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
+             { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
+             { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}];
+
+
+//1.
+let obj = {};
+
+for(let i = 0; i < users.length; i++) {
+    let fullName = users[i].firstName + " " + users[i].lastName;
+    obj[fullName] = users[i].role;
+}
+console.log(obj);
