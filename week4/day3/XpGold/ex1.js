f26e754aa0c64f712d0483774d7a50f0593e8718
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-09-16 12:44:50 +0200

    commit ex1Gold

---
+++ week4/day3/XpGold/ex1.js
@@ -0,0 +1,27 @@
+//Exercise 1 : print Full Name
+
+//1.
+function printFullName(obj){
+    const {first, last} = obj;
+    return `Your full name is ${first} ${last}`;
+}
+
+console.log(printFullName({first: 'Elie', last:'Schoppik'}));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
