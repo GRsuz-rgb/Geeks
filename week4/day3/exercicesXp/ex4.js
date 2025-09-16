32e643415e80a92f025447583a7284ebd830453b
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-09-16 10:37:36 +0200

    commit ex4Xp

---
+++ week4/day3/exercicesXp/ex4.js
@@ -0,0 +1,20 @@
+//Exercise 4 : Person class
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const member = new Person('John');
+console.log(typeof member);
+
+//output : object , because member is an instance of class Person and in js all instances are from the class Object  
+
+
+
+
+
+
+
+
