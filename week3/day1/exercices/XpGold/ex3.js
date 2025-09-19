f5eeecd78fc88a927ce2f0de673f5a269e9614b3
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-09-19 11:16:03 +0200

    commit ex3Gold

---
+++ week3/day1/exercices/XpGold/ex3.js
@@ -0,0 +1,27 @@
+//Exercise 3 : Playing with numbers
+
+let age = [20,5,12,43,98,55];
+
+let sum = 0;
+for (let i = 0; i < age.length; i++) {
+    sum += age[i];
+}
+
+console.log("Sum of all numbers in the age table : ", sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
