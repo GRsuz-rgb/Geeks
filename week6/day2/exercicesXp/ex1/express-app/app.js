09343434e4ff55e75ab6147aceed642bf2e507bb
GRsuz-rgb <ilham.gourou-etu@etu.univh2c.ma>
2025-12-26 11:21:24 +0100

    ex1Xp_Routes

--- week6/day2/exercicesXp/ex1/express-app/app.js
+++ week6/day2/exercicesXp/ex1/express-app/app.js
@@ -5,8 +5,7 @@ const app = express();
 const PORT = 3000;
 
 // Mount the router
-app.use("/", router);
-
+app.use("/", router); //tells Express to use the routes defined in your router for any incoming requests to your server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
